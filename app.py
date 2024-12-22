17913a4de63745f758c6592e217a336800936470
Santoshsai17 <85979977+Santoshsai17@users.noreply.github.com>
2024-12-22 20:23:40 +0000

    updated html page

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+JOBS = [{
+    'id': 1,
+    'title': 'Data Analyst',
+    'location': 'Bengaluru, India'
+}, {
+    'id': 2,
+    'title': 'Softwarw engineer',
+    'location': 'mumbai'
+}, {
+    'id': 3,
+    'title': 'Teting',
+    'location': 'delhi, India'
+}]
 
 
 @app.route('/')
 def hello_world():
-  return 'Hello, World!'
+  return render_template('home.html', jobs=JOBS)
 
 
 if __name__ == '__main__':
